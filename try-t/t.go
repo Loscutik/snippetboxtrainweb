8574ab3976fc7bd62866eb86b168ea85c1f201ed
loscutik <loscutik@gmail.com>
2023-01-13 18:51:20 +0000

    test dir try-t

---
+++ try-t/t.go
@@ -0,0 +1,7 @@
+package try
+
+import "fmt"
+
+func TestRoot() {
+	fmt.Println("I'm in package try in the dir try-t")
+}
--- try/t.go
+++
@@ -1,7 +0,0 @@
-package try
-
-import "fmt"
-
-func TestRoot() {
-	fmt.Println("I'm in package try")
-}
